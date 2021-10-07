83594e0dce01f4bd3443885e07ad0246e1a63d87
LakshBharani <laksh.bharani@gmail.com>
2021-10-08 00:34:17 +0530

    .

--- lib/screens/new_vacc.dart
+++ lib/screens/new_vacc.dart
@@ -43,9 +43,11 @@ class _NewVaccinePageState extends State<NewVaccinePage> {
   final firestoreInstance = FirebaseFirestore.instance;
   final _formKey = GlobalKey<FormState>();
   bool loading = false;
+  bool isImageAdded = false;
 
   String vaccine = '';
   String doses = '';
+  String imageUrl = '';
   @override
   Widget build(BuildContext context) {
     return loading
@@ -56,84 +58,171 @@ class _NewVaccinePageState extends State<NewVaccinePage> {
               backgroundColor: Colors.blue[800],
               title: Text("Add a record"),
             ),
-            body: Center(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                            hintText: 'Vaccine Name'),
-                        validator: (val) =>
-                            val!.isEmpty ? 'Enter Vaccine name' : null,
-                        onChanged: (val) {
-                          setState(() {
-                            vaccine = val;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        keyboardType: TextInputType.numberWithOptions(),
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Doses'),
-                        validator: (val) =>
-                            val!.isEmpty ? 'Enter number of doses' : null,
-                        onChanged: (val) {
-                          setState(() {
-                            doses = (val);
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      FlatButton(
-                        color: Colors.blue[800],
-                        textColor: Colors.white,
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            setState(() {
-                              loading = true;
-                            });
-                            var firebaseUser =
-                                FirebaseAuth.instance.currentUser;
-                            firestoreInstance
-                                .collection("users")
-                                .doc(firebaseUser?.uid)
-                                .collection("vaccines")
-                                .doc(vaccine.toLowerCase())
-                                .set({
-                              "vaccName":
-                                  vaccine.substring(0, 1).toUpperCase() +
-                                      vaccine.substring(1),
-                              "date": currentDate,
-                              "doses": doses,
-                              "searchKey":
-                                  vaccine.substring(0, 1).toUpperCase(),
-                            }).then((_) {
-                              print("success!");
-                              Navigator.pop(context);
+            body: SingleChildScrollView(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                  child: Form(
+                    key: _formKey,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(
+                                hintText: 'Vaccine Name'),
+                            validator: (val) =>
+                                val!.isEmpty ? 'Enter Vaccine name' : null,
+                            onChanged: (val) {
                               setState(() {
-                                loading = false;
+                                vaccine = val;
                               });
-                              _showToast(context);
-                            });
-                          }
-                        },
-                        child: Text("Submit"),
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.numberWithOptions(),
+                            decoration:
+                                textInputDecoration.copyWith(hintText: 'Doses'),
+                            validator: (val) =>
+                                val!.isEmpty ? 'Enter number of doses' : null,
+                            onChanged: (val) {
+                              setState(() {
+                                doses = (val);
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            height: 150,
+                            width: 290,
+                            color: Colors.amber[50],
+                            child: isImageAdded
+                                ? MaterialButton(
+                                    onPressed: () {},
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [],
+                                    ),
+                                  )
+                                : MaterialButton(
+                                    onPressed: () {},
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      // ignore: prefer_const_literals_to_create_immutables
+                                      children: [
+                                        Icon(
+                                          Icons.camera_alt_rounded,
+                                          color: Colors.blue[800],
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text('Click here to upload image'),
+                                      ],
+                                    ),
+                                  ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FlatButton(
+                                padding: EdgeInsets.only(left: 6, right: 6.5),
+                                color: Colors.blue[800],
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  setState(() {
+                                    isImageAdded = false;
+                                  });
+                                },
+                                child: Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    Icon(
+                                      Icons.replay_circle_filled_outlined,
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text("Retake Image"),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              FlatButton(
+                                padding: EdgeInsets.only(right: 0, left: 7),
+                                color: Colors.blue[800],
+                                textColor: Colors.white,
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    setState(() {
+                                      loading = true;
+                                    });
+                                    var firebaseUser =
+                                        FirebaseAuth.instance.currentUser;
+                                    firestoreInstance
+                                        .collection("users")
+                                        .doc(firebaseUser?.uid)
+                                        .collection("vaccines")
+                                        .doc(vaccine.toLowerCase())
+                                        .set({
+                                      "vaccName": vaccine
+                                              .substring(0, 1)
+                                              .toUpperCase() +
+                                          vaccine.substring(1),
+                                      "date": currentDate,
+                                      "doses": doses,
+                                      "searchKey":
+                                          vaccine.substring(0, 1).toUpperCase(),
+                                      "imageUrl": imageUrl
+                                    }).then((_) {
+                                      print("success!");
+                                      Navigator.pop(context);
+                                      setState(() {
+                                        loading = false;
+                                      });
+                                      _showToast(context);
+                                    });
+                                  }
+                                },
+                                child: Row(
+                                  // ignore: prefer_const_literals_to_create_immutables
+                                  children: [
+                                    Text("Submit"),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios_rounded,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
