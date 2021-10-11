a440c54856ad4c2c0b07e9e3750fc014a71f6360
LakshBharani <laksh.bharani@gmail.com>
2021-10-12 02:11:38 +0530

    Update new_vacc.dart

--- lib/screens/new_vacc.dart
+++ lib/screens/new_vacc.dart
@@ -3,7 +3,6 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
